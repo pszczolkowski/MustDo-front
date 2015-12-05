9b177d4b2d9d8154cceebf53632472e04196f7bc
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-12-05 17:02:20 +0100

    Prompt user logins while adding new user to team

--- app/scripts/team/team-details.controller.js
+++ app/scripts/team/team-details.controller.js
@@ -9,13 +9,16 @@
 		'$scope',
 		'toaster',
 		'Team',
+		'User',
 		'team',
 		'identity'];
 
-	function TeamDetailsController($scope, toaster, Team, team, identity) {
+	function TeamDetailsController($scope, toaster, Team, User, team, identity) {
 		$scope.team = teamWithoutLoggedUser(team);
 		$scope.removeMember = removeMember;
 		$scope.addMember = addMember;
+
+		loadUsers();
 
 
 		function teamWithoutLoggedUser(team) {
@@ -29,6 +32,26 @@
 			}
 
 			return newTeam;
+		}
+
+		function loadUsers() {
+			User.query().$promise
+				.then(function (users) {
+					$scope.users = getUsersWithoutTeamMembersFrom(users);
+				});
+		}
+
+		function getUsersWithoutTeamMembersFrom(users) {
+			for (var i = 0; i < $scope.team.members.length; i++) {
+				for (var j = 0; j < users.length; j++) {
+					if ($scope.team.members[i].id === users[j].id) {
+						users.splice(j, 1);
+						break;
+					}
+				}
+			}
+
+			return users;
 		}
 
 		function removeMember(member) {
@@ -46,6 +69,7 @@
 				teamId: $scope.team.id
 			}).$promise.then(function (team) {
 				$scope.team = teamWithoutLoggedUser(team);
+				$scope.users = getUsersWithoutTeamMembersFrom($scope.users);
 			});
 		}
 
@@ -56,10 +80,11 @@
 
 			Team.addMember({
 				teamId: $scope.team.id,
-				login: $scope.newMember
+				userId: $scope.newMember.id
 			}).$promise.then(function () {
 				toaster.pop('success', 'Team member added');
 				reloadTeam();
+				$scope.newMember = null;
 			}, function () {
 				toaster.pop('error', 'User with given login doesn\'t exist');
 			});
